20973b805ec5083b360dc95237b43db8112b93a1
Bunmi Akande <bunmiakande57@gmail.com>
2025-06-26 17:39:00 -0700

    Another soln using a pointer moving through the array

--- Arrays/27_removeElement.py
+++ Arrays/27_removeElement.py
@@ -5,23 +5,35 @@ The order of the elements may be changed.
 Then return the number of elements in nums which are not equal to val.
 '''
 class Solution(object):
+    # def removeElement(self, nums, val):
+    #     count = 0
+    #     val_index = []
+    #     #loop through array 
+    #     for i in range(len(nums)):
+    #         #if element is val, store index
+    #         if nums[i] == val:
+    #             val_index.append(i)
+    #         else:
+    #             count += 1 #count element that is = to val 
+    #             #replace position of element with position of val
+    #             if len(val_index) >= 1:
+    #                 nums[val_index[0]] = nums[i]
+    #                 val_index.pop(0)
+    #                 #add position of moved variable
+    #                 val_index.append(i)
+    #     return count,nums
+    
+
+    #solution 2 
     def removeElement(self, nums, val):
-        count = 0
-        val_index = []
-        #loop through array 
+        k = 0
+
         for i in range(len(nums)):
-            #if element is val, store index
-            if nums[i] == val:
-                val_index.append(i)
-            else:
-                count += 1 #count element that is = to val 
-                #replace position of element with position of val
-                if len(val_index) >= 1:
-                    nums[val_index[0]] = nums[i]
-                    val_index.pop(0)
-                    #add position of moved variable
-                    val_index.append(i)
-        return count,nums
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k, nums
     
 
 nums = [0,1,2,2,3,0,4,2]
